3d2363551503dee10675d1b6feb9f0c62bd7aa8b
wathika-eng <wathika02@gmail.com>
2024-05-09 13:33:57 +0300

    fixes

--- src/Pages/utils.js/index.js
+++ src/Pages/utils.js/index.js
@@ -1,10 +1,10 @@
-export async function fetchRecipes(filter){
-    const {query,limit} = filter;
-    const url =`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}&from=0&to=${limit}`;
-    
-    const response = await fetch(url) 
-    
-    const data = await response.json();
-    console.log(data.meals)
-    return data.meals;
+export async function fetchRecipes(filter) {
+	const { query, limit } = filter;
+	const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}&from=0&to=${limit}`;
+
+	const response = await fetch(url);
+
+	const data = await response.json();
+	console.log(data.meals);
+	return data.meals;
 }
